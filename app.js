91c789ebf70b63a23216866bc98bcade44013b5a
Dilou89 <odil_cadiou@orange.fr>
2017-01-08 22:50:41 +0100

    vers &

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('have fun !');
+$(document).ready(function(){
+
+$('input:text').keyup(function(e) {    
+   if(e.keyCode == 13) {
+
+var fM = $("#first_name").val();
+var lM=$("#last_name").val();
+var city =$("#city:text").val();
+
+console.log(fM);
+
+console.log(lM);
+	
+console.log(city);
+
+var username = fM + ""+""+lM;
+console.log(username);
+
+};	
+
+});
+});
